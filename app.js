ba4e529bc06b0cb2a890ef7ea91e32783795bdc9
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-03-13 18:42:10 +0100

    Parsing of CSV files and reading thereof finished.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const loadData = require('./assets/scripts/loadData')
 const express = require('express')
 const app = express()
+let db = {};
 
 // Setting the template engine (ejs)
 app.set('view engine', 'ejs');
@@ -11,5 +13,15 @@ app.use(express.static('assets'));
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+// Builds database.
+// Horrible on memory but confines our project to JS only (no use of external DBs).
+setTimeout(() => {
+    db.UserDB = loadData.buildUserDB()
+    db.movies = loadData.movies;
+    db.links = loadData.links;
+    db.ratings = loadData.ratings;
+}, 500); // Arbitrary interval. The above takes time to load depending on machine speed -
+         // this is to accommodate that.
 
 server = app.listen(3000);
