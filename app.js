c20aa14d24c61767604fc1bb4509d3e51013124c
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-03-17 14:00:32 +0100

    Experiment - finding total ratings depending on user

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ setTimeout(() => {
     db.movies = loadData.movies;
     db.links = loadData.links;
     db.ratings = loadData.ratings;
+
+    // Finds total amount of ratings between all users with more than 50 ratings given.
+    /* let filteredArray = [];
+    let totalRatings = 0;
+    for (let i = 1; i < db.UserDB.length; i++) {
+        if (db.UserDB[i].length >= 50) {
+            filteredArray.push(db.UserDB[i]);
+            totalRatings += db.UserDB[i].length;
+        }
+    }
+    console.log(filteredArray.length, totalRatings); */
 }, 500); // Arbitrary interval. The above takes time to load depending on machine speed -
          // this is to accommodate that.
 
