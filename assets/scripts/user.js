998129a63271dfe89b792834f7da9b557cf4b072
Philip Christian Greve <39656769+philip-greve@users.noreply.github.com>
2020-03-18 11:15:38 +0100

    Beginning of connect with friends

--- assets/scripts/user.js
+++ assets/scripts/user.js
@@ -3,6 +3,7 @@ class User {
     constructor(userID){
         this.userID = userID;
         this.userName = "Your name";
+        this.friends = [];  // Contains ID of friends
     }
     changeUserName(newUserName) {
         this.userName = newUserName;
@@ -15,5 +16,9 @@ console.log("User ID:" + user1.userID);
 console.log("User name:" + user1.userName);
 user1.changeUserName("Bob");
 console.log("User name:" + user1.userName);
+
+user1.friends.push(63);
+user1.friends.push(42);
+console.log("User 1's friends:" + user1.friends);
 
 
