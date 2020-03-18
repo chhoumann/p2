1ca6a8aedbca07eb914908dbb5c307b6066c0efd
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-03-18 15:16:16 +0100

    Cleaned & added startup function to build user & test DB

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const loadData = require('./assets/scripts/loadData')
-const express = require('express')
-const app = express()
+const dataHandler = require('./assets/scripts/dataHandler');
+const express = require('express');
+const app = express();
+const arrayOfUserIds = [3, 5, 4, 2, 1]; // Used for testing. Users 1-5 are test-users.
+let testGroup;
 let db = {};
 
 // Setting the template engine (ejs)
@@ -22,28 +24,15 @@ app.get('/movie-rec', (req, res) => {
     res.render('movie-rec');
 
 });
+
 // Builds database.
 // Horrible on memory but confines our project to JS only (no use of external DBs).
-// Remember that it is the pointers to the objects which are copied, not the objects themselves.
-setTimeout(() => {
-    db.UserDB = loadData.buildUserDB()
-    db.movies = loadData.movies;
-    db.links = loadData.links;
-    db.ratings = loadData.ratings;
-
-    // Finds total amount of ratings between all users with more than 50 ratings given.
-    /* let filteredArray = [];
-    let totalRatings = 0;
-    for (let i = 1; i < db.UserDB.length; i++) {
-        if (db.UserDB[i].length >= 50) {
-            filteredArray.push(db.UserDB[i]);
-            totalRatings += db.UserDB[i].length;
-        }
-    }
-    console.log(filteredArray.length, totalRatings); */
-}, 500); // Arbitrary interval. The above takes time to load depending on machine speed -
-         // this is to accommodate that.
-
+const startup = async () => { 
+    db.userDB = await dataHandler.buildMovieLensUserDB();
+    testGroup = dataHandler.groupUsers(db.userDB, 5, arrayOfUserIds);
+    // console.log(testGroup);
+}
+startup();
 
 let port = process.env.PORT;
 if (port == null || port == "") {
