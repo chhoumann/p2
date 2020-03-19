c41f6a98ebfaefcd8b91705092cdb0b63c934423
Christopher-Jensen <cjen@student.aau.dk>
2020-03-19 18:23:01 +0100

    Hjælp Christian

--- assets/scripts/add-form.js
+++ assets/scripts/add-form.js
@@ -1,4 +1,17 @@
-let aForm = document.querySelector("#add-person");
-aForm.addEventListener("click", ()=>{
-    console.log(aForm.innerHTML);
+let add = document.querySelector("#addPerson");
+add.addEventListener("click", ()=>{
+    let newLabel = document.createElement("LABEL");
+    let newInput = document.createElement("INPUT")
+    let formID = document.createAttribute("ID");
+    newLabel.setAttribute("type", "text");
+    newLabel.setAttribute("id", "ID-box");
+    formID.value = "ID-box";
+    newLabel.innerHTML = "Enter ID <br>";
+    document.getElementById("groupMember").insertBefore(newLabel, document.getElementById("addPerson"));
+    document.getElementById("groupMember").insertBefore(newInput, document.getElementById("addPerson"));
 })
+
+// let sub = document.querySelector("#submitForm");
+// sub.addEventListener("click", () => {
+//     sub.submit();
+// })
