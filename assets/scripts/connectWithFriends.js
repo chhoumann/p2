c96dcda616fe27ebabc0b5e5f59d3684b43800b9
Philip Christian Greve <39656769+philip-greve@users.noreply.github.com>
2020-03-20 11:47:30 +0100

    ConnectWithFriends

--- assets/scripts/connectWithFriends.js
+++ assets/scripts/connectWithFriends.js
@@ -1,21 +1,45 @@
+'use strict';
+
 // Logic to find a user in userDB and and add to friend list
 // Logic to accept a friend request(?)
 
+
+
+let User = require('./user');
+let someUser = new User.User(67);
+
+// Global array of added friends, containing userID's
 let addedFriends = [];
 
+// The import is successful and can print data about this object
+console.log(someUser);
+someUser.changeUserName("Bob");
+console.log(someUser);
 
 // A function for alerting the friend was added
 function addToFriendList() {
-    if (isValidID){
+    let friendID = document.getElementById("fname").value;
+    friendID = parseInt(friendID);
+    if (isValidID(friendID)){
         alert("Friend with ID: " + document.getElementById("fname").value + " was added to your friends list");
-        let friendID = document.getElementById("fname").value;
         addedFriends.push(friendID); // FIXME: Later this should be added to our user object's array
+    }
+    else {
+        alert("Error - not a valid ID");
+        alert(someUser.userName);
+        console.log(someUser.userName);
     }
     
 }
 
 // Validate whether the input ID is valid
-// Start with just whether the input is a possible number
-function isValidID() {
-    return true;
+function isValidID(ID) {
+    if (Number.isInteger(ID)){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
+
+
--- assets/scripts/user.js
+++ assets/scripts/user.js
@@ -1,9 +1,11 @@
+'use strict';
+
 // The blueprint of our user
 class User {
     constructor(userID){
         this.userID = userID;
         this.userName = "Your name";
-        this.friends = [];  // Contains ID of friends
+        this.friends = [];  // Contains ID's of friends
     }
     changeUserName(newUserName) {
         this.userName = newUserName;
@@ -11,10 +13,10 @@ class User {
 }
 
 // Our user of the site
-let ourUser = new User(0000);
+let ourUser = new User(0);
 
 // Testing changing user attributes
-let user1 = new User(0001);
+let user1 = new User(1);
 console.log("User ID:" + user1.userID);
 console.log("User name:" + user1.userName);
 user1.changeUserName("Bob");
@@ -24,4 +26,4 @@ user1.friends.push(63);
 user1.friends.push(42);
 console.log("User 1's friends:" + user1.friends);
 
-
+module.exports = {User};
