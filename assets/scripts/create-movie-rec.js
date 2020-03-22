797c678297dd2dfb5777440f5027d7c7384dbbe8
KrisMP99 <kpede19@student.aau.dk>
2020-03-22 22:23:54 +0100

    Added a little code

---
+++ assets/scripts/create-movie-rec.js
@@ -0,0 +1,10 @@
+let User = require('./user');
+
+function getMovieRec(e){
+    e.preventDefault();
+    
+    let userID1 = document.getElementById("ID-box-1").value;
+    let userID2 = document.getElementById("ID-box-2");
+    
+
+}
