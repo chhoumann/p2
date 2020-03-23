d42ec640f05eb3d4bf86f543ce45c3c68e7b9afb
Rune Holmgaard Andersen <runeandersen@Runes-MacBook-Pro.local>
2020-03-23 10:24:18 +0100

    lidt nnice

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const arrayOfUserIds = [3, 5, 4, 2, 1]; // Used for testing. Users 1-5 are test-users.
 let testGroup;
+let bodyParser = require('body-parser'); 
+let urlencodedParser = bodyParser.urlencoded({ extended: false })  
 let db = {};
 
 // Setting the template engine (ejs)
@@ -28,6 +30,31 @@ app.get('/movie-rec', (req, res) => {
     res.render('movie-rec');
 
 });
+
+app.get('/createAccount', (req, res) => {
+    res.render('createAccount');
+
+});
+
+
+
+// Create application/x-www-form-urlencoded parser  
+
+app.use(express.static('public'));  
+app.get('/scripts/Profile.ejs', function (req, res) {  
+   res.sendFile( "/scripts" + "/" + "Profile.ejs" );  
+})  
+app.post('./createAccount', urlencodedParser, function (req, res) {  
+   // Prepare output in JSON format  
+   response = {  
+       uname:req.body.uname,  
+       pword:req.body.pword,
+       gender:req.body.gender  
+   };  
+   console.log(response);  
+   res.end(JSON.stringify(response));  
+})  
+
 
 // Builds database.
 // Horrible on memory but confines our project to JS only (no use of external DBs).
