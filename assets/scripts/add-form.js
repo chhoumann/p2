cf58bf9b30e2ed83d646f7a30831e1a5cb93845d
KrisMP99 <kpede19@student.aau.dk>
2020-03-23 13:32:48 +0100

    Giver de nye inputs et unikt ID

--- assets/scripts/add-form.js
+++ assets/scripts/add-form.js
@@ -1,14 +1,27 @@
 let add = document.querySelector("#addPerson");
+
+/* Variable for incrementing ID for input boxes
+   Begins at 2 because of the two already existing inputs */
+let input_box_ID = 2;
 add.addEventListener("click", ()=>{
     let newLabel = document.createElement("LABEL");
     let newInput = document.createElement("INPUT")
     let formID = document.createAttribute("ID");
+
+
     newLabel.setAttribute("type", "text");
     newLabel.setAttribute("id", "ID-box");
+
+    newInput.setAttribute("id", `ID-box-${input_box_ID + 1}`);
+    //Increment variable to give new ID to input box 
+    input_box_ID += 1;
+
     formID.value = "ID-box";
-    newLabel.innerHTML = "Enter ID <br>";
+
+    newLabel.innerHTML = "<br>Enter ID <br>";
     document.getElementById("groupMember").insertBefore(newLabel, document.getElementById("addPerson"));
     document.getElementById("groupMember").insertBefore(newInput, document.getElementById("addPerson"));
+
 })
 
 // let sub = document.querySelector("#submitForm");
