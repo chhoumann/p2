4d4d6ac66a6f95cb1d20d1e28c3507be8e966449
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-03-24 00:04:39 +0100

    Routes moved & intitialization made better

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const dataHandler = require('./assets/scripts/data/dataHandler');
+const utility = require('./utility');
+const bodyParser = require('body-parser'); 
+const routes = require('./routes.js')
 const express = require('express');
-const app = express();
+const {performance} = require('perf_hooks');
 const arrayOfUserIds = [3, 5, 4, 2, 1]; // Used for testing. Users 1-5 are test-users.
+const SEPARATOR = "----------------------------------------------";
+const app = express();
 let testGroup;
-let bodyParser = require('body-parser'); 
 let urlencodedParser = bodyParser.urlencoded({ extended: false })  
 let db = {};
 
@@ -14,32 +18,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('assets'));
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('test');
+app.use('/', routes);
 
-});
-app.get('/profile', (req, res) => {
-    res.render('profile');
-  });
-
-app.get('/connect-with-friends', (req, res) => {
-  res.render('connect-with-friends');
-});
-
-app.get('/movie-rec', (req, res) => {
-    res.render('movie-rec');
-
-});
-
-app.get('/updateProfile', (req, res) => {
-    res.render('updateProfile');
-
-});
-
-app.get('/createAccount', (req, res) => {
-    res.render('createAccount');
-
-});
 // Dette sender informationerne fra formen fra http://localhost:8000/Profile til http://localhost:8000/something
 // Der er redirect efter submit, men dette logger desuden requesten til consollen samt skriver det på '/something'.
 // TODO: Der burde tilføjes funktionalitet som skriver alt dette til en json fil (vores database over brugere) og tjekker duplicates.
@@ -52,22 +32,29 @@ app.post('/something', urlencodedParser, function (req, res) {
        gender:req.body.gender  
    };  
    console.log(response);
-   res.end(JSON.stringify(response)); // Her skrives til '/something'  
-})  
+   res.end(JSON.stringify(response)); // Her skrives til '/something' som klienten modtager
+});
 
-
-// Builds database.
+// Builds database & test group.
 // Horrible on memory but confines our project to JS only (no use of external DBs).
-const startup = async () => { 
-    db.userDB = await dataHandler.buildMovieLensUserDB();
-    testGroup = dataHandler.groupUsers(db.userDB, 5, arrayOfUserIds);
-    // console.log(testGroup);
+const initialize = async () => {
+    console.log(SEPARATOR);
+    try {
+        let startTime = performance.now();
+        db.userDB = await dataHandler.buildMovieLensUserDB();
+        utility.printTestAndTime("UserDB", db.userDB, startTime);
+        
+        startTime = performance.now();
+        testGroup = dataHandler.groupUsers(db.userDB, 5, arrayOfUserIds);
+        utility.printTestAndTime("Testgroup", testGroup, startTime);
+    }
+    catch(error) { utility.logError(error) };
+    console.log(SEPARATOR);
 }
-startup();
 
+// Heroku support.
 let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 8000;
-}
+if (port == null || port == "") { port = 8000; }
 
+initialize();
 server = app.listen(port);
