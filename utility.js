8fdc160ac927de3e3ee16e6bc22d22904f495a19
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-03-24 00:05:00 +0100

    Added assorted utility functions

---
+++ utility.js
@@ -0,0 +1,13 @@
+const {performance} = require('perf_hooks');
+
+// Terminal colors
+const RED_COLOR_TERMINAL = "\x1b[31m";
+const GREEN_COLOR_TERMINAL = "\x1b[32m";
+const RESET_COLOR_TERMINAL = "\x1b[0m";
+
+module.exports.successMessage = (item) => { console.log(GREEN_COLOR_TERMINAL, `✔ ${item} successfully loaded.`, RESET_COLOR_TERMINAL) };
+module.exports.errorMessage = (item) => { return `❌ ${item} not loaded.` }; // First letter should be capitalized.
+module.exports.testIfUndefined = (item, itemName) => { if (item === undefined) { throw this.errorMessage(itemName) } else { this.successMessage(itemName); return true; } };
+module.exports.logError = (error) => { console.log(RED_COLOR_TERMINAL, error, RESET_COLOR_TERMINAL) };
+module.exports.timeFunction = (item, initialTime) => { console.log(`   - Loading ${item} took ${((performance.now() - initialTime)/1000).toPrecision(2)} seconds.`); }
+module.exports.printTestAndTime = (itemName, item, startTime) => { if (this.testIfUndefined(item, itemName)) this.timeFunction(itemName, startTime); };
