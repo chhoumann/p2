5636b78825faef2533a43a8cb90bfc79e46bad6b
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-03-23 18:15:48 +0100

    Gave function a more relevant name and indented

--- assets/scripts/updateProfile.js
+++ assets/scripts/updateProfile.js
@@ -1,8 +1,10 @@
-function myFunction() {
+// ! Christian: Har givet den et lidt mere relevant navn og lavet indentation.
+// Bruges i Profile.ejs
+function showPassword() {
         let x = document.getElementById("pword");
         if (x.type === "password") {
-        x.type = "text";
+            x.type = "text";
         } else {
-        x.type = "password";
+            x.type = "password";
         }
   }
