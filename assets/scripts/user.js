4ba1f5cfcef83541283846938b4a635d937432b0
Philip Christian Greve <39656769+philip-greve@users.noreply.github.com>
2020-03-23 16:21:30 +0100

    Added to user class

--- assets/scripts/user.js
+++ assets/scripts/user.js
@@ -1,29 +1,49 @@
 'use strict';
 
-// The blueprint of our user
+// The blueprint of our user.
 class User {
     constructor(userID){
         this.userID = userID;
         this.userName = "Your name";
-        this.friends = [];  // Contains ID's of friends
+        this.friends = [];  // Contains ID's of friends.
     }
     changeUserName(newUserName) {
         this.userName = newUserName;
     }
+    friendsWith(friendID) {
+        for (let i = 0; i < this.friends.length; i++){
+            if (this.friends[i] === friendID){
+                return true;
+            }
+        }
+        return false;
+    }
+    addToFriendList(friendID){
+        if (isValidID(friendID) && !this.friendsWith(friendID)) {
+            this.friends.push(friendID);
+            console.log("Friend with ID: " + friendID + " was added to friends list");
+        }
+        else
+            console.log("Error: " + friendID + " is not a valid ID. Or already a friend.");
+    }
 }
 
-// Our user of the site
-// let ourUser = new User(0);
-
-// Testing changing user attributes
-// let user1 = new User(1);
-// console.log("User ID:" + user1.userID);
-// console.log("User name:" + user1.userName);
-// user1.changeUserName("Bob");
-// console.log("User name:" + user1.userName);
-
-// user1.friends.push(63);
-// user1.friends.push(42);
-// console.log("User 1's friends:" + user1.friends);
-
 module.exports.createUser = new User(0);
+
+// The HTML validates input is a number. Here we other conditions.
+function isValidID(id) {
+    if (id >= 1 && id <=1000 && Number.isInteger(id)) {
+        return true;
+    }
+};
+
+
+
+let myUser = new User(5);
+myUser.changeUserName("Kurt");
+myUser.addToFriendList(50);
+myUser.addToFriendList(51);
+myUser.addToFriendList(51);
+myUser.addToFriendList(5);
+console.log(myUser.userName);
+console.log(myUser.friends);
