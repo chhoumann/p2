dc68dbe00f248cdcb67114aef2c65ad7be3edf61
KrisMP99 <kpede19@student.aau.dk>
2020-03-25 16:00:28 +0100

    Now possible to get all the entered users IDs stored in an array for furture use

--- assets/scripts/create-movie-rec.js
+++ assets/scripts/create-movie-rec.js
@@ -1,10 +1,22 @@
-let User = require('./user');
+// Get movie recommendation for multiple users
 
 function getMovieRec(e){
     e.preventDefault();
     
-    let userID1 = document.getElementById("ID-box-1").value;
-    let userID2 = document.getElementById("ID-box-2");
+    // Get number of input fields
+    let form = document.getElementById("groupMember");
+    let numOfIDs = form.getElementsByTagName("input").length;
     
+    // Variable for userIDs
+    let userID = [];
 
+    // Store userIDs in array
+    for(let x = 1; x <= numOfIDs; x++){
+        userID[x-1] = document.getElementById(`ID-box-${x}`).value;
+    }
+
+    // TODO: Add some kind of check that the given users exists?
+
+    // TODO: Get the different users movie preferences and calculate a recommendation 
+    
 }
