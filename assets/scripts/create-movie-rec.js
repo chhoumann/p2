1dfb586584cb3df4e4752c55b48a581e1e314369
KrisMP99 <kpede19@student.aau.dk>
2020-03-29 12:41:30 +0200

    Added userID check on input from user

--- assets/scripts/create-movie-rec.js
+++ assets/scripts/create-movie-rec.js
@@ -1,5 +1,6 @@
 // Get movie recommendation for multiple users
 
+
 function getMovieRec(event){
     event.preventDefault();
     
@@ -8,14 +9,24 @@ function getMovieRec(event){
     let numOfIDs = form.getElementsByTagName("input").length;
     
     // Variable for userIDs
-    let userID = [];
+    let userIDArray = [];
 
     // Store userIDs in array
     for(let x = 1; x <= numOfIDs; x++){
-        userID[x-1] = document.getElementById(`ID-box-${x}`).value;
+        let userID = parseInt(document.getElementById(`ID-box-${x}`).value);
+
+        if(isValidID(userID)) {
+            userIDArray.push(userID);
+            console.log("Sucessfully added friends: " + userIDArray);
+        } else {
+            console.log("Error: Not valid userID: " + userID);
+        }
+        
     }
 
     // TODO: Add some kind of check that the given users exists?
+    // Stolen from "connectWithFriends.js"
+    function isValidID(id) { if (id >= 1 && id <=1000 && Number.isInteger(id)) return true; };
 
     // TODO: Get the different users movie preferences and calculate a recommendation 
 }
