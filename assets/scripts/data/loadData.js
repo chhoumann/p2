fa2faf7af33a6fb7c09b2d0c28ca6b09f419117c
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-03-30 15:21:21 +0200

    Skelner nu mellem filtyper i loadData
    Co-authored-by: RuneHolmgaardAndersen <rhan19@student.aau.dk>
    Co-authored-by: christopher-jensen <christopher.colberg@gmail.com>

--- assets/scripts/data/loadData.js
+++ assets/scripts/data/loadData.js
@@ -17,8 +17,12 @@ const RATING_DB_PATH = './db/ratingDB.json';
 const MOVIELENS_USER_DB_PATH = './db/movieLensUserDB.json';
 const MOVIE_DB_PATH = './db/movieDB.json';
 const TESTGROUP_PATH = './db/testGroup.json';
+const USER_DB_PATH = './db/dbOfUsers.json';
 
-async function getData(path) {
+const CSV_TYPE = 'CSV';
+const JSON_TYPE = 'JSON';
+
+async function getData(path, type) {
     let result;
     try {
         let startTime = performance.now();
@@ -29,14 +33,16 @@ async function getData(path) {
         utility.logError(`${error.name} in loading ${error.path}!`);
         return;
     }
-    return neatCsv(result);
+    if (type === CSV_TYPE) return neatCsv(result);
+    if (type === JSON_TYPE) return result;
 };
 
-module.exports.getMovieData = async() => { return await getData(MOVIES_CSV_PATH) };
-module.exports.getLinkData = async() => { return await getData(LINKS_CSV_PATH) };
-module.exports.getRatingData = async() => { return await getData(RATINGS_CSV_PATH) };
-module.exports.getTestGroupData = async() => { return await getData(TESTGROUP_PATH) };
+module.exports.getMovieData = async() => { return await getData(MOVIES_CSV_PATH, CSV_TYPE) };
+module.exports.getLinkData = async() => { return await getData(LINKS_CSV_PATH, CSV_TYPE) };
+module.exports.getRatingData = async() => { return await getData(RATINGS_CSV_PATH, CSV_TYPE) };
+module.exports.getTestGroupData = async() => { return await getData(TESTGROUP_PATH, JSON_TYPE) };
 
-module.exports.getMovieDB = async() => { return await getData(MOVIE_DB_PATH) };
-module.exports.getRatingDB = async() => { return await getData(RATING_DB_PATH) };
-module.exports.getMovieLensUserDB = async() => { return await getData(MOVIELENS_USER_DB_PATH) };
+module.exports.getMovieDB = async() => { return await getData(MOVIE_DB_PATH, JSON_TYPE) };
+module.exports.getRatingDB = async() => { return await getData(RATING_DB_PATH, JSON_TYPE) };
+module.exports.getMovieLensUserDB = async() => { return await getData(MOVIELENS_USER_DB_PATH, JSON_TYPE) };
+module.exports.getUserDB = async() => { return await getData(USER_DB_PATH, JSON_TYPE) };
