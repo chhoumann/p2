2f959dea26e64d7305fd1a4fe0c72890d8b4336a
Rune Holmgaard Andersen <runeandersen@Runes-MacBook-Pro.local>
2020-03-31 15:24:49 +0200

    fikset updateProfile så showpassword virker igen

--- assets/scripts/updateProfile.js
+++ assets/scripts/updateProfile.js
@@ -1,7 +1,7 @@
 // ! Christian: Har givet den et lidt mere relevant navn og lavet indentation.
 // Bruges i Profile.ejs
 function showPassword() {
-        let x = document.getElementById("pword");
+        let x = document.getElementById("password");
         if (x.type === "password") {
             x.type = "text";
         } else {
