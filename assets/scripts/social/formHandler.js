d63c24d6cc940d1c24b9734dff94cf1e6a7d06a9
KrisMP99 <kpede19@student.aau.dk>
2020-04-01 13:36:15 +0200

    Removed redundant code

--- assets/scripts/social/formHandler.js
+++ assets/scripts/social/formHandler.js
@@ -7,12 +7,7 @@ add.addEventListener("click", ()=>{
     let newLabel = document.createElement("LABEL");
     let newInput = document.createElement("INPUT");
 
-    //What is this used for?
-    let formID = document.createAttribute("ID");
-    formID.value = "ID-box";
-
     newLabel.setAttribute("type", "text");
-    newLabel.setAttribute("id", "ID-box");
 
     newInput.setAttribute("id", `ID-box-${input_box_ID + 1}`);
     newInput.setAttribute("name", "ID");
