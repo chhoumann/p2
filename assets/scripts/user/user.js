a3b2cd3d8a19447b7196980e1f21a2321ce0dfb3
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-04-17 02:18:30 +0200

    Removed unused code

--- assets/scripts/user/user.js
+++ assets/scripts/user/user.js
@@ -7,73 +7,11 @@ const fs = require('fs');
 
 // The blueprint of our user.
 module.exports.User = class User {
-    constructor(id, username, password){
+    constructor(id, username){
         this.id = id;
         this.username = username;
-        this.password = password; // Currently plain text. Might want to add encryption or something...
-        this.friends = [];  // Contains ID's of friends.
+        this.friends = [];  
         this.moviePreferences = []; // ? Suggestion: array of objects containing movie IDs and ratings of each movie.
-    }
-    changeUserName(newUserName) {
-        this.userName = newUserName;
-    }
-    friendsWith(friendID) {
-        for (let i = 0; i < this.friends.length; i++){
-            if (this.friends[i] === friendID){
-                return true;
-            }
-        }
-        return false;
-    }
-    addToFriendList(friendID){
-        if (isValidID(friendID, "user") && !this.friendsWith(friendID)) {
-            this.friends.push(friendID);
-            console.log("Friend with ID: " + friendID + " was added to friends list");
-        }
-        else
-            console.log("Error: " + friendID + " is not a valid ID. Or is already a friend.");
-    }
-    addToMoviePreferences(movieID, userRating) {
-        if (isValidMovieID(movieID, "movie")) {  // Are not checking here for whether ID has already been added
-            this.moviePreferences.push({id: movieID, rating: userRating});
-        }
-    }
-}
-
-//module.exports.createUser = new User(0);
-
-// Switch on different ID's under different circumstances
-function isValidID(id, typeOfID) {
-    let isValid = false;
-    switch(typeOfID) {
-        case "user":
-            isValid = isValidFriendID(id);
-            break;
-        case "movie":
-            isValid = isValidMovieID(id);
-            break;
-    }
-    return isValid;
-};
-
-function isValidFriendID(id) {
-    if (id >= 1 && id <=1000 && Number.isInteger(id)) {
-        return true;
-    }
-}
-
-function isValidMovieID(id) {
-    if (id >= 1 && id <=1000 && Number.isInteger(id)) { // Probably check for a different number interval
-        return true;
-    }
-}
-
-function showPassword() {
-    let x = document.getElementById("password");
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
     }
 }
 
@@ -82,7 +20,7 @@ module.exports.createUser = async function(req) {
 
     // Prepare user in proper format  
     const user = dataHandler.formatUser(req, userDB["users"].length);
-    
+
     // Checking for duplicates
     if (utility.usernameDuplicateChecker(userDB["users"], user.username)) {
         // Add to DB and save to file.
