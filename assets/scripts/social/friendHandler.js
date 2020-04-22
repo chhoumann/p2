2207aafca8ed7bedae7a191dc9139cbde2321c7e
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-04-22 13:33:21 +0200

    Fikset bugs med friends systemet og gjort kode renere

--- assets/scripts/social/friendHandler.js
+++ assets/scripts/social/friendHandler.js
@@ -8,10 +8,12 @@ let app = new Vue({
         loggedIn: isLoggedIn(),
         username: "",
         friendsList: [],
-        errorSelf: false,
-        errorDupe: false,
-        errorInput: false,
-        errorNoUserWithName: false,
+        error: '',
+    },
+    created() {
+        if (this.isLoggedIn()) {
+            (async () => await this.updateFriendsListOnPage())();
+        }
     },
     methods: {
         login: function () {
@@ -35,6 +37,7 @@ let app = new Vue({
                 localStorage.setItem("loggedIn", true);
                 localStorage.setItem("username", username);
                 app.username = username;
+                this.updateFriendsListOnPage();
             } else {
                 sweetAlert('Error', `Login unsuccessful. Try again.`, 'error');
                 localStorage.setItem("loggedIn", false);
@@ -68,80 +71,64 @@ let app = new Vue({
                 }
             }
         },
+        updateFriendsListOnPage: async function() {
+            this.friendsList = await this.fetchFriendsList(this.fetchAndSetUsername());
+        },
+        fetchAndSetUsername: function() {
+            const username = localStorage.getItem('username');
+            if ( this.username !== username ) this.username = username;
+            return username;
+        },
+        addFriend: async function() {
+            const requestBy = this.username;
+            const addName = friendNameInput.value;
+        
+            // Error handling
+            if (await this.handleErrors(requestBy, addName)) return;
+        
+            // Send to server
+            const {data: {valid}} = await axios.get('/addFriend', {params: { requestBy, addName }});
+        
+            // Depending on the response, it'll append the friend added to the friends list on the page.
+            if (valid) {
+                app.friendsList.push({name: addName});
+                app.error = '';
+            } else { app.error = 'No user with that name exists.'; }
+        },
+        // Fetch the friend list of logged-in user.
+        fetchFriendsList: async function(user) {
+            const response = await axios.get('/fetchFriends', {params: {user}});
+            const friendsList = response.data;
+            return friendsList;
+        },
+        // Handle if user is adding itself
+        selfAddErrorHandler: function(requestBy, addName) {
+            if (requestBy === addName) {
+                app.error = "You can't add yourself as a friend.";
+                return true;
+            }
+        },
+        // Handle if user is adding someone who is already a friend
+        dupeAddErrorHandler: async function(requestBy, addName) {
+            const FL = await this.fetchFriendsList(requestBy);
+            const findDupe = FL.find(user => { return user.name === addName })
+            if (findDupe) {
+                app.error = 'You are already friends.';
+                return true;
+            }
+        },
+        friendNameInputErrorHandler: function() {
+            if (friendNameInput.value === "" ) {
+                app.error = 'No friend name entered.'
+                return true;
+            }
+        },
+        handleErrors: async function(requestBy, addName) {
+            let errorOccurred = false;
+            if (this.friendNameInputErrorHandler()) errorOccurred = true;
+            if (this.selfAddErrorHandler(requestBy, addName)) errorOccurred = true;
+            if (await this.dupeAddErrorHandler(requestBy, addName)) errorOccurred = true;
+            return errorOccurred;
+        }
     }
 })
-
-function fetchAndSetUsername() {
-    const username = localStorage.getItem('username');
-    if ( app.username !== username ) app.username = username;
-    return username;
-}
-
-// Fetch the friend list of logged-in user.
-async function fetchFriendsList(user) {
-    const response = await axios.get('/fetchFriends', {params: {user}});
-    const friendsList = response.data;
-    return friendsList;
-};
-
-// Uses Vue functionality to update list of friends on page.
-const updateFriendsListOnPage = async () => { app.friendsList = await fetchFriendsList(fetchAndSetUsername())};
-
-// Handle if user is adding itself
-const selfAddErrorHandler = (requestBy, addName) => {
-    app.errorDupe = false; // <- Bug fix: if you add dupe and then yourself it shows two error messages. Thanks || operator.
-    if (requestBy === addName) {
-        app.errorSelf = true;
-        return true;
-    } else { app.errorSelf = false };
-};
-
-// Handle if user is adding someone who is already a friend
-const dupeAddErrorHandler = async (requestBy, addName) => {
-    const FL = await fetchFriendsList(requestBy);
-    const findDupe = FL.find(user => { return user.name === addName })
-    if (findDupe) {
-        app.errorDupe = true;
-        return true;
-    } else { app.errorDupe = false }
-};
-
-const friendNameInputErrorHandler = () => {
-    if (friendNameInput.value === "" ) {
-        app.errorInput = true;
-        return true;
-    } else { app.errorInput = false };
-}
-
-const handleErrors = async (requestBy, addName) => {
-    let errorOccurred = false;
-    if (friendNameInputErrorHandler()) errorOccurred = true;
-    if (selfAddErrorHandler(requestBy, addName)) errorOccurred = true;
-    if (await dupeAddErrorHandler(requestBy, addName)) errorOccurred = true;
-    return errorOccurred;
-}
-
-async function addFriend() {
-    const requestBy = fetchAndSetUsername();
-    const addName = friendNameInput.value;
-
-    // Error handling
-    if (await handleErrors(requestBy, addName)) return;
-
-    // Send to server
-    const response = await axios.get('/addFriend', {params: { requestBy, addName }});
-    
-    console.log(response["data"].valid) // test
-
-    // Depending on the response, it'll append the friend added to the friends list on the page.
-    if (response["data"].valid) {
-        app.friendsList.push({name: addName})
-    } else { app.errorNoUserWithName = true }
-}
-
-if (isLoggedIn()) {
-    updateFriendsListOnPage();
-    const friendNameInput = document.querySelector('#friendNameInput');
-    const addFriendBtn = document.querySelector('#addFriendBtn');
-    addFriendBtn.addEventListener('click', addFriend);
-}
